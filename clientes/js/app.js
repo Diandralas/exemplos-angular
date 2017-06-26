64ea0a254aea68622cb3c7deadd668c91aabb514
Bruno <brunoapimentel@gmail.com>
2017-06-26 15:30:43 -0300

    Ex4: cadastro clientes

--- clientes/js/app.js
+++ clientes/js/app.js
@@ -3,4 +3,9 @@ var app = angular.module('clientes', []);
 app.controller('ClientesController', function($scope){
     $scope.clientes = [];
     $scope.dados = {};
+
+    $scope.cadastrar = function(){
+        $scope.clientes.push($scope.dados);
+        $scope.dados = {};
+    }
 });
