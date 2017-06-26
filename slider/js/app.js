c3e93d059f14fb61414ce57e372a27fbf66c9bba
Bruno <brunoapimentel@gmail.com>
2017-06-26 13:33:03 -0300

    Ex3: slider funcionando

--- slider/js/app.js
+++ slider/js/app.js
@@ -1,1 +1,22 @@
 var app = angular.module('slider', []);
+
+app.controller('CarrosselController', function($scope, $interval){
+    $scope.imagens = [
+        {src: "img/cerveja1.jpg", texto: "Cervejinha Boa"},
+        {src: "img/cerveja2.jpg", texto: "Cervejinha Gelada"},
+        {src: "img/cerveja3.jpg", texto: "Cervejinha Maneira"}
+    ];
+
+    var indice = 0;
+    $scope.imagemAtiva = $scope.imagens[indice];
+
+    $interval(function(){
+        indice++;
+
+        if(indice == $scope.imagens.length){
+            indice = 0;
+        }
+
+        $scope.imagemAtiva = $scope.imagens[indice];
+    }, 3000);
+})
