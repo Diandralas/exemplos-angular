bd30449f4a93b2e9782f0875d5dde65918d6aa9b
Bruno <brunoapimentel@gmail.com>
2017-06-26 11:22:25 -0300

    Ex2: Adicionado cores

--- quadrados/js/app.js
+++ quadrados/js/app.js
@@ -1,12 +1,23 @@
 var app = angular.module('quadrados', []);
 
 app.controller('QuadradosController', function($scope){
+    var cores = ['#f442aa', '#ee41f4', '#b241f4'];
+
     $scope.quadrados = [
-        {cor: "blue"},
-        {cor: "red"},
-        {cor: "yellow"},
-        {cor: "pink"},
-        {cor: "green"},
-        {cor: "red"}
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]},
+        {cor: cores[0]}
     ];
 });
